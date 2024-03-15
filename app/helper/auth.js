e29e0b98d4464b493ecbb35974b24fa17b7e24bb
vidhirathod29 <vidhirathod.shivinfotech@gmail.com>
2024-03-15 15:48:31 +0530

    make user module api

--- app/helper/auth.js
+++ app/helper/auth.js
@@ -1,18 +1,19 @@
-var mongoose = require('mongoose');
+const { binary } = require("joi");
+var mongoose = require("mongoose");
 
 var auth = new mongoose.Schema({
-    firstname: {type:String},
-    lastname:{type :String},
-    gender:{
-        type:String,
-        enum:['male','female','others']
-    },
-    hobby:{type :Array},
-    city:{type:String},
-    mobile: { type: Number },
-    email: { type: String },
-    password: { type: String },
-    image: { type: String }
-})
-var authModel= mongoose.model('Auth',auth);
-module.exports={authModel};
+  firstName: { type: String },
+  lastName: { type: String },
+  gender: {
+    type: String,
+    enum: ["male", "female", "others"],
+  },
+  hobby: { type: Array },
+  city: { type: String },
+  mobile: { type: String },
+  email: { type: String },
+  password: { type: String },
+  image: { type: String},
+});
+var authModel = mongoose.model("Auth", auth);
+module.exports = { authModel };
